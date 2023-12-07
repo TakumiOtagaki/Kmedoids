06a8163bcce04b04366c0fbb982d41264a214145
otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2023-12-08 01:50:40 +0900

    advanced option implemented

--- kmedoids-parallel.py
+++ kmedoids-parallel.py
@@ -1,5 +1,6 @@
-# python Kmedoids.py --num_core 64 --input_distmat dist.csv --dist_type (triu|tril|sym) \
-#                    --output_medoids medoids.csv --output_label labels.csv --num_clusters 2 --max_iter 1000
+# python Kmedoids.py --num_core 4 --input_distmat dist.csv --dist_type (triu|tril|sym) \
+#                    --output_medoids medoids.csv --output_label labels.csv --num_clusters 2 --max_iter 1000 \
+#                    --verbose --random_seed 0
 
 import argparse
 import numpy as np
@@ -8,7 +9,6 @@ import time
 import sys
 import os
 import multiprocessing as mp
-
 
 
 def parse_args():
@@ -30,6 +30,10 @@ def parse_args():
                         help='Verbose mode', default=False)
     parser.add_argument('--max_iter', type=int, default=300,
                         help='Maximum number of iterations')
+    parser.add_argument('--av_cpu', default=False, action='store_true',
+                        help='Check available CPU. If True, the program will exit after checking available CPU')
+    parser.add_argument('--random_seed', type=int, default=0,
+                        help='Random seed.')
     return parser.parse_args()
 
 def read_distmat(distmat_file, dist_type):
@@ -63,7 +67,7 @@ def update_medoids(distmat, labels, medoids, i):
         # medoids: medoids, list
         # i: cluster index
     # return: medoids[i]
-
+    
     cluster = np.where(labels == i)[0]
     distmat_sub = distmat[cluster][:, cluster]
     # medoids[i] = cluster[np.argmin(np.sum(distmat_sub, axis=1))]
@@ -71,7 +75,7 @@ def update_medoids(distmat, labels, medoids, i):
     return cluster[np.argmin(np.sum(distmat_sub, axis=1))]
 
 
-def kmedoids(distmat, num_clusters, num_thread, verbose, max_iter=300):
+def kmedoids(distmat, num_clusters, num_thread, verbose, max_iter, random_seed):
     # kmedoids clustering
         # distmat: distance matrix (symmetry), ndarray
         # num_clusters: number of clusters
@@ -84,16 +88,18 @@ def kmedoids(distmat, num_clusters, num_thread, verbose, max_iter=300):
     for i in range(distmat.shape[0]):
         labels[i] = np.argmin(distmat[i, medoids])
 
-    # start kmedoids
+    if verbose: print('Initialization done'); iter_span = (max_iter // 5) if max_iter > 5 else 1
 
+    # start kmedoids
     for iter in range(max_iter):
-        if verbose and iter % 100 == 0:
-            print(f"Iteration {iter}: {iter * 1.0 / args.max_iter * 100} %")
+        if verbose and (iter % iter_span == 0 or iter == max_iter - 1):
+            print(f"Iteration {iter}: {iter * 1.0 / max_iter * 100} %")
         # update medoids
         # use multiprocessing to speed up
         pool = mp.Pool(processes=num_thread)
         results = [pool.apply_async(update_medoids, args=(distmat, labels, medoids, i)) for i in range(num_clusters)]
-        medoids_new = [p.get() for p in results]
+        medoids_new = np.array([p.get() for p in results])
+        pool.close()
         # for i in range(num_clusters):
         #     cluster = np.where(labels == i)[0]
         #     distmat_sub = distmat[cluster][:, cluster]
@@ -105,13 +111,15 @@ def kmedoids(distmat, num_clusters, num_thread, verbose, max_iter=300):
         # use multiprocessing to speed up
         pool = mp.Pool(processes=num_thread)
         results = [pool.apply_async(np.argmin, args=(distmat[i, medoids_new],)) for i in range(distmat.shape[0])]
-        labels = [p.get() for p in results]
+        labels = np.array([p.get() for p in results])
+        pool.close()
 
+        medoids = medoids_new.copy()
         # check convergence
         if np.array_equal(labels, labels_old):
             if verbose: print('Converged')
             break
-
+    if verbose: print('Not converged')
     return medoids, labels
 
 def input_validation(args):
@@ -124,23 +132,48 @@ def input_validation(args):
         raise ValueError('distance matrix file not found')
     if args.num_clusters <= 0:
         raise ValueError('num_clusters must be greater than 0')
+    if args.max_iter <= 0:
+        raise ValueError('max_iter must be greater than 0')
+    if args.num_points <= 0:
+        raise ValueError('num_points must be greater than 0')
+    if args.num_points > args.num_thread:
+        raise ValueError('num_points must be less than or equal to num_thread')
+    if type(args.random_seed) != int:
+        raise ValueError('random_seed must be integer')
     return True
+
+def available_cpu():
+    # check available cpu
+    # return: number of available cpu
+    return mp.cpu_count()
 
 def main():
     # parse arguments
     args = parse_args()
 
+    if args.av_cpu: print(f"Available CPU: {available_cpu()}\nexit.") ; return
+    
     # read distance matrix
-    if args.verbose: print('Reading distance matrix...')
+    if args.verbose: print('Reading distance matrix...'); start = time.time()
     distmat = read_distmat(args.input_distmat, args.dist_type)
-    if args.verbose: print('Done'); print(f"Distance matrix shape: {distmat.shape}")
+    if args.verbose: print('Done'); print(f"Distance matrix shape: {distmat.shape}"); print(f"Time elapsed: {time.time() - start} s")
+
+    # if args.num_points > args.num_thread: args.num_thread = args.num_points
+    if distmat.shape[0] < args.num_thread: 
+        args.num_thread = distmat.shape[0]
+        print("Warning: num_points > num_thread, set num_thread = num_points")
 
     # kmedoids clustering
-    medoids, labels = kmedoids(distmat, args.num_clusters, args.num_thread, args.verbose, args.max_iter)
+    if args.verbose: print('Clustering...'); start = time.time()
+    medoids, labels = kmedoids(distmat, args.num_clusters, args.num_thread, args.verbose, args.max_iter, args.random_seed)
+    if args.verbose: print('Done'); print(f"Time elapsed: {time.time() - start} s")
 
     # save medoids and labels
+    if args.verbose: print('Saving medoids and labels...'); start = time.time()
     np.savetxt(args.output_medoids, medoids, fmt='%d', delimiter=',')
     np.savetxt(args.output_label, labels, fmt='%d', delimiter=',')
+    if args.verbose: print('Done'); print(f"Time elapsed: {time.time() - start} s")
+    
 
 if __name__ == '__main__':
     main()
